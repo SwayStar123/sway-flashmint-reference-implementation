8884c70d8d06596c867e8490bed74ffe9f7e266b
simonr0204 <simon.roberts@fuel.sh>
2023-01-24 17:37:16 -0500

    initial commit

---
+++ flashloan/tests/harness.rs
@@ -0,0 +1,104 @@
+use fuels::{
+    prelude::*,
+    tx::ContractId,
+};
+
+macro_rules! fn_selector {
+    ( $fn_name: ident ( $($fn_arg: ty),* )  ) => {
+         ::fuels::core::code_gen::function_selector::resolve_fn_selector(stringify!($fn_name), &[$( <$fn_arg as ::fuels::core::Parameterize>::param_type() ),*]).to_vec()
+    }
+}
+macro_rules! calldata {
+    ( $($arg: expr),* ) => {
+        ::fuels::core::abi_encoder::ABIEncoder::encode(&[$(::fuels::core::Tokenizable::into_token($arg)),*]).unwrap().resolve(0)
+    }
+}
+
+
+
+// Load abi from json
+abigen!(
+    Flashloan,
+    "out/debug/flashloan-abi.json"
+);
+
+abigen!(
+    Callee,
+    "../flashloanCallee/out/debug/flashloanCallee-abi.json"
+);
+
+
+async fn get_contract_instances() -> (Flashloan, ContractId) {
+    // Launch a local network and deploy the contract
+    let mut wallets = launch_custom_provider_and_get_wallets(
+        WalletsConfig::new(
+            Some(1),             /* Single wallet */
+            Some(1),             /* Single coin (UTXO) */
+            Some(1_000_000_000), /* Amount per coin */
+        ),
+        None,
+        None,
+    )
+    .await;
+    let wallet = wallets.pop().unwrap();
+
+    let id = Contract::deploy(
+        "../flashloan/out/debug/flashloan.bin",
+        &wallet,
+        TxParameters::default(),
+        StorageConfiguration::with_storage_path(Some(
+            "../flashloan/out/debug/flashloan-storage_slots.json"
+                .to_string(),
+        )),
+    )
+    .await
+    .unwrap();
+    let instance = Flashloan::new(id.clone(), wallet.clone());
+
+
+    let callee_id = Contract::deploy(
+        "../flashloanCallee/out/debug/flashloanCallee.bin",
+        &wallet,
+        TxParameters::default(),
+        StorageConfiguration::with_storage_path(Some(
+            "../flashloanCallee/out/debug/flashloanCallee-storage_slots.json"
+                .to_string(),
+        )),
+    )
+    .await
+    .unwrap();
+
+
+    (instance, callee_id.into())
+}
+
+
+
+#[tokio::test]
+async fn can_flashloan() {
+
+    let (instance, target) = get_contract_instances().await;
+
+    let function_selector = fn_selector!(my_func(bool));
+    let calldata = calldata!(false);
+
+    let tx = instance.methods().flashloan(100, target, function_selector, calldata, true, 1_000_000).set_contracts(&[target.into()]).tx_params(TxParameters::default());
+    let _result = tx.call().await.unwrap();
+}
+
+
+#[tokio::test]
+#[should_panic(expected = "LoanNotRepaid")]
+async fn reverts_if_not_fully_repaid() {
+
+    let (instance, target) = get_contract_instances().await;
+
+    let function_selector = fn_selector!(my_func(bool));
+    let calldata = calldata!(true);
+
+    let tx = instance.methods().flashloan(100, target, function_selector, calldata, true, 1_000_000).set_contracts(&[target.into()]).tx_params(TxParameters::default());
+    let _result = tx.call().await.unwrap();
+}
+
+
+
---
+++ flashloanCallee/src/main.sw
@@ -0,0 +1,27 @@
+contract;
+
+// A contract with a function that can only be called if some value was forwarded
+// The contract then forces the forwarded amount back to the sender, optionally underpaying by 1
+
+use std::context::msg_amount;
+use std::call_frames::msg_asset_id;
+use std::token::force_transfer_to_contract;
+
+abi Callee {
+    fn my_func(underpay: bool);
+}
+
+impl Callee for Contract {
+    fn my_func(underpay: bool) {
+        if msg_amount() < 100 {
+            revert(42);
+        }; 
+        if underpay {
+            force_transfer_to_contract(msg_amount() - 1, msg_asset_id(), msg_asset_id());
+        }
+        else {
+            force_transfer_to_contract(msg_amount(), msg_asset_id(), msg_asset_id());
+        };
+    }
+
+}
